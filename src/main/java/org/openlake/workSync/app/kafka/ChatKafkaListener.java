07ea1764f8e44141594ca61064571ae7d558296f
Sumit Pathak <sumitpathak2002@gmail.com>
2025-07-10 19:11:27 +0530

    added logging for kafka

--- src/main/java/org/openlake/workSync/app/kafka/ChatKafkaListener.java
+++ src/main/java/org/openlake/workSync/app/kafka/ChatKafkaListener.java
@@ -3,6 +3,8 @@ package org.openlake.workSync.app.kafka;
 import lombok.RequiredArgsConstructor;
 import org.openlake.workSync.app.dto.MessageResponseDTO;
 import org.openlake.workSync.app.service.ChatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChatKafkaListener {
     private final ChatService chatService;
+    private static final Logger logger = LoggerFactory.getLogger(ChatKafkaListener.class);
 
     @KafkaListener(topicPattern = "chat-.*", groupId = "chat-consumer-group", containerFactory = "kafkaListenerContainerFactory")
     public void listen(MessageResponseDTO message) {
-        chatService.broadcastMessage(message);
+        try {
+            chatService.broadcastMessage(message);
+        } catch (Exception ex) {
+            logger.error("Failed to broadcast chat message", ex);
+        }
     }
 }
